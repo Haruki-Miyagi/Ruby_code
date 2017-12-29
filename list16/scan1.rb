b389b1babd7f162e76083c7d15efe047dc4cad01
Haruki <haruki.miyagi1112@gmail.com>
2017-12-29 23:56:24 +0900

    list16_正規表現（Regexp）クラス

--- list16/scan1.rb
+++ list16/scan1.rb
@@ -1,9 +1,18 @@
 "abracatabra".scan(/.a/) do |matched|
   p matched
 end
+puts "--------------"
 
 "abracatabra".scan(/(.)(a)/) do |matched|
   p matched
 end
+puts "--------------"
 
+"abracatabra".scan(/(.)(a)/) do |a, b|
+  p a+"-"+b
+end
+puts "---------------"
 
+str = "http://www.ruby-lang.org/ja/"
+%r|http://([^/]*)/| =~ str
+print "server address: ", $1, "\n"
