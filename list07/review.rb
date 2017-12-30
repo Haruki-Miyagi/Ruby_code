60dea8fa33bc526887dfaf00bbf8f502af3979f9
Haruki <haruki.miyagi1112@gmail.com>
2017-12-30 20:31:10 +0900

    list07_review.rb 復習途中(メソッド)

---
+++ list07/review.rb
@@ -0,0 +1,53 @@
+puts "インスタンスメソッド"
+
+p "1,2,3,4,5".split(",")
+p [1,2,3,4,5].index(2)
+p 1000.to_s
+p Time.now.to_s
+
+puts "クラスメソッド"
+p Array["a", "b", "c"] #["a", "b", "c"]という配列を生成する
+
+puts "関数的メソッド"
+print "hello!"
+sleep(2)
+
+puts "----------------"
+p print("i:")
+
+puts "メソッドの定義"
+
+def hello(name = "haruki")
+  puts "Hello #{name}!"
+end
+hello()
+hello("Ruby")
+
+puts "メソッドの戻り値"
+def volume(x,y,z)
+  return x * y * z
+end
+p volume(2,3,4) #=> 24
+puts "大小を表す a=>10, b =>48 大きいのは？"
+def max (a,b)
+  if a > b
+    a
+  else
+    b
+  end
+end
+p max(10,48)
+
+puts "loop メソッド"
+def myloop
+  while true
+    yield
+  end
+end
+
+num = 1
+myloop do
+  puts "num is #{num}"
+  break if num > 10
+  num *= 2
+end
