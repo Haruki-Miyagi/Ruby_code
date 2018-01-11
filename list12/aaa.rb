2ef8e78c122d304c4f825b9905edfad406537cd1
Haruki <haruki.miyagi1112@gmail.com>
2018-01-11 11:11:02 +0900

    list12_数値の変換

--- list12/aaa.rb
+++ list12/aaa.rb
@@ -6,3 +6,12 @@ c = a + b
 p c
 p c.to_f
 p [c.numerator, c.denominator]
+
+puts "-------------"
+num = 10
+print("元の値は", num, "です:")
+print("Float型に変換すると", num.to_f, "となります")
+puts "----"
+num = 14.87
+print("元の値は", num, "です:")
+print("Integer型に変換すると", num.to_i, "となります")
