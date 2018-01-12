38de14262884122b9014d80e9eaafa8a450795a1
Haruki <haruki.miyagi1112@gmail.com>
2018-01-12 23:52:18 +0900

    list13_配列クラス

--- list13/list.rb
+++ list13/list.rb
@@ -24,3 +24,16 @@ list = ["a", "b", "c", "d"]
 list.each_with_index do |elem, i|
   print i+1,"番目の要素は",elem,"です。\n"
 end
+
+puts "---------------"
+ary1 = [1, 2, 3, 4, 5]
+ary2 = [10, 20, 30, 40, 50]
+ary3 = [100, 200, 300, 400, 500]
+ 
+i = 0
+result = []
+while i < ary1.length
+  result << ary1[i] + ary2[i] + ary3[i]
+  i += 1
+end
+p result  #=> [111, 222, 333, 444, 555]
