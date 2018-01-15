9600acec018d510129170d28f2d660175fed6642
Haruki <haruki.miyagi1112@gmail.com>
2018-01-15 17:49:59 +0900

    list16_(gub,sub)

--- list16/scan1.rb
+++ list16/scan1.rb
@@ -16,3 +16,28 @@ puts "---------------"
 str = "http://www.ruby-lang.org/ja/"
 %r|http://([^/]*)/| =~ str
 print "server address: ", $1, "\n"
+
+puts "--------------"
+string = "hello, hello, hello Ruby"
+p string.gsub(/hello/, "morning")
+
+puts "文字列を変数で渡す"
+string = "hello, hello, hello Ruby"
+regx = "hello"
+p string.gsub(/#{regx}/, "morning")
+
+puts "ハッシュで置き換える"
+hash = {'r'=>'Ruby', 'b'=>'プログラミング'}
+p "This is rb language".gsub(/[rb]/, hash)
+
+puts "ブロックで置き換え"
+string = "hello, ruby"
+p string.gsub(/\w+/) {|s| s.upcase }
+
+puts "----------------"
+string = "hello, hello, hello Ruby"
+p string.sub(/hello/, "morning")
+
+puts "---"
+string = "hello, hello, hello,ruby"
+p string.sub(/hello/) {|s| s.upcase}
