5a2f5b2e25f4f01af18e81a4fc884190dad6fcd8
Haruki <haruki.miyagi1112@gmail.com>
2018-01-20 23:35:45 +0900

    list16(復習)_scan1.rb

--- list16/scan1.rb
+++ list16/scan1.rb
@@ -21,16 +21,21 @@ puts "--------------"
 string = "hello, hello, hello Ruby"
 p string.gsub(/hello/, "morning")
 
-puts "文字列を変数で渡す"
+puts "hoge => HELLO 復習"   #OK!
+hello = "hoge, hoge, hoge, Ruby"
+hoge = "hoge"
+p hello.gsub(/#{hoge}/, 'HELLO')
+
+puts "文字列を変数で渡す"     #OK!
 string = "hello, hello, hello Ruby"
 regx = "hello"
 p string.gsub(/#{regx}/, "morning")
 
-puts "ハッシュで置き換える"
+puts "ハッシュで置き換える" #OK!
 hash = {'r'=>'Ruby', 'b'=>'プログラミング'}
 p "This is rb language".gsub(/[rb]/, hash)
 
-puts "ブロックで置き換え"
+puts "ブロックで置き換え" #OK!
 string = "hello, ruby"
 p string.gsub(/\w+/) {|s| s.upcase }
 
@@ -41,3 +46,9 @@ p string.sub(/hello/, "morning")
 puts "---"
 string = "hello, hello, hello,ruby"
 p string.sub(/hello/) {|s| s.upcase}
+
+
+
+puts "hoge hoge　復習"
+hoge = "hoge, hoge, hoge, Ruby"
+p string.sub(/hoge/) {|s| s.upcace}
