f2ba47098f2fc9f6fc022495bbb6bf3acc5e69f0
Haruki <haruki.miyagi1112@gmail.com>
2018-01-21 21:40:34 +0900

    list16_(new,split)

--- list16/scan3.rb
+++ list16/scan3.rb
@@ -6,4 +6,14 @@ puts "=> quote（eacapeメソッドの別名）"
 p Regexp.quote("a[b]c{d}")
 p Regexp.new(Regexp.quote("a[b]c[d]"))
 
-puts "=>  "
+puts "=> new（Regexpクラスのオブジェクトを生成する） "
+p Regexp.new("abcdefg")
+p Regexp.new("abcdefg", Regexp::IGNORECASE)
+p Regexp.new("abcdefg", Regexp::MULTILINE)
+p Regexp.new("abcdefg", Regexp::EXTENDED)
+
+puts " => split, split!（マッチした部分を分割し配列で返す）"
+string = "Hello, Ruby, World"
+p string.split(/\s*,\s*/) # 「,」で分割
+
+puts "=> "
