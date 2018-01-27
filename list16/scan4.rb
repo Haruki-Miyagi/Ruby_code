6623691cc2de725f480e092ee18b3015d9545a08
haruki <Haruki.Miyagi1112@gmail.com>
2018-01-27 23:44:25 +0900

    list16(文字クラスの省略記法)

---
+++ list16/scan4.rb
@@ -0,0 +1,27 @@
+string = "9a"
+p string.gsub(/\wa/, "morning")
+string = "Za"
+p string.gsub(/\wa/, "morning")
+
+puts "反対にa-z、A-Z、0-9のうちのいずれの1文字にも一致しない"
+string = "暑a"
+p string.gsub(/\Wa/, "morning")
+string = "\na"
+
+puts '空白文字に\f,\n,\r,\tに一致'
+string = "\ra"
+p string.gsub(/\sa/, "morning")
+string = "\na"
+p string.gsub(/\sa/, "morning")
+
+puts '反対に空白文字に\f,\n,\r,\tに一致しない'
+string = "Aa"
+p string.gsub(/\Sa/, "morning")
+string = "0a"
+p string.gsub(/\Sa/, "morning")
+
+puts '10進数の数字に一致する場合は\dを使用します。'
+string = "#a"
+p string.gsub(/\Da/, "morning")
+string = "9a"
+p string.gsub(/\Da/, "morning")
