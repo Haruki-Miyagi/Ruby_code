d81db2701c37398ee0d6facc369c382c3b2b9d3c
haruki <Haruki.Miyagi1112@gmail.com>
2018-04-03 20:13:43 +0900

    真偽判定

--- list13/index.rb
+++ list13/index.rb
@@ -12,3 +12,15 @@ p lang
 p lang[-1]
 p lang[-2]
 p lang[1, 4]
+p lang[1, -2]
+
+# zero?================---
+p 0.to_i.zero?
+p nil.to_i.zero?
+p 'sanple'.to_i.zero?
+p ''.to_i.zero?
+# false=---------------
+p 1.to_i.zero?
+
+p nil.nil?
+p "".empty?
