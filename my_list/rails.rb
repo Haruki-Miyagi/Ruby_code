967fc74983e8d0c877edbec044a13c0f043748a0
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-04-30 10:14:49 +0900

    代入

--- my_list/rails.rb
+++ my_list/rails.rb
@@ -23,11 +23,11 @@ puts message
 puts "---"
 puts message("")
 puts "------------------"
- 
+
 p  %w[a b c].map { |char| char.upcase }
 p  %w[A B C].map { |char| char.downcase }
 puts "------------------"
-s = "foobar" 
+s = "foobar"
 puts s.class
 puts s.class.superclass
 puts s.class.superclass.superclass
@@ -62,6 +62,32 @@ puts alert_for(:notice)
 puts alert_for('alert-success')
 puts alert_for(:success)
 
+# ====================================================-
+# 多重代入
+a, b = 1, 2
+p a # => 1
+p b # => 2
+a, b = [1, 2, 3]
+p a # => 1
+p b # => 2
+# ====
+a, *b = [1, 2, 3]
+p a # => 1
+p b # => [2, 3]
+# ====
+a, b, c = [1, 2]
+p a # => 1
+p b # => 2
+p c # => nil
+# ===
+# 入れ替え
+a, b = b, a
+# ===
+# 自己代入
+p a += 1   # a = a + 1
+puts "test b ?"
+p b
+p b ||= 2  # b = b || 2
 
-
-
+c = 1
+p c ||= 4 # c = 1
